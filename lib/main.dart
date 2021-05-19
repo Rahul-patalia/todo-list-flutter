e5bec28bf2c1d3979508d2ee16def9342846cae0
Rahul-Patalia <pataliarahul97@gmail.com>
2021-05-19 16:42:55 +1000

    Last commit with all the changes

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,25 @@ void main() => runApp(MaterialApp
   home: todoList(),
 ));
 
+
 class todoList extends StatefulWidget {
   @override
   _todoListState createState() => _todoListState();
+
 }
 
 class _todoListState extends State<todoList> 
 {
-
   List<Todo> Todolist = [ 
     Todo(Item: "Finish Assignment", Catagory: "University Work"),
-    Todo(Item: "Take walk", Catagory: "General"),
+    Todo(Item: "Take a walk", Catagory: "General"),
     Todo(Item: "Job at 10 a.m.", Catagory: "part-time job"),
+    Todo(Item: "Some Work", Catagory: "about work"),
+    Todo(Item: "Some Work", Catagory: "about work"),
+    Todo(Item: "Some Work", Catagory: "about work"),
+    Todo(Item: "Some Work", Catagory: "about work"),
     ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +37,8 @@ class _todoListState extends State<todoList>
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
+
+      
 
       body: SingleChildScrollView
       (
@@ -47,11 +55,6 @@ class _todoListState extends State<todoList>
             }
           )).toList(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton
-      (
-        onPressed: () {},
-        child: Icon(Icons.add_circle_outlined)
       ),
     );
   }

632ea4f02aa03aba31da45fbdaf123acdd776380
Rahul-Patalia <pataliarahul97@gmail.com>
2021-05-19 11:23:47 +1000

    added model class and made some changes in main.dart file

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:dynemic_widget_app/quote.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp
+(
+  home: QuateList(),
+));
+
+class QuateList extends StatefulWidget {
+  @override
+  _QuateListState createState() => _QuateListState();
+}
+
+class _QuateListState extends State<QuateList> 
+{
+
+  List<Quote> quotes = [ 
+    Quote(quoteText: "Code is like humor. When you have to explain it, it’s bad", author: "Cory House"),
+    Quote(quoteText: "Experience is the name everyone gives to their mistakes.", author: "Oscar Wilde"),
+    Quote(quoteText: "Believe in you!, only and only you can do it", author: "Rahul Patalia"),
+    ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      appBar: AppBar
+      (
+        title: Text("Dynemic Widgets App"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+
+      body: Column
+      (
+        children: quotes.map((quote) => Text("${quote.quoteText} - ${quote.author}")).toList(),
+      ),
+    );
+  }
+}
+
---
+++ lib/quote.dart
@@ -0,0 +1,9 @@
+import 'package:dynemic_widget_app/main.dart';
+
+class Quote
+{
+    String quoteText;
+    String author;
+
+    Quote({ this.quoteText, this.author });
+}
